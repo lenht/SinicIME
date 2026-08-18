b8e9084be866e254dc49044a5f7a68ca1e8d471b
Lena Tran <ltran95@gatech.edu>
2026-08-18 12:38:31 +0700

    Merged imemobile.js into ime.js

--- Scripts/ime.js
+++ Scripts/ime.js
@@ -32,6 +32,7 @@ var contqSz = 0;
 var contrSz = 0;
 var contail = "";
 var conqueue = "";
+var curtxtPadlength = 0; // tracks txtPad length between keystrokes for touch-input diffing (see txtPadKeyInput)
 
 var lentype = 0;
 var carpos = -1;
@@ -252,21 +253,11 @@ function txtPadKeyPressed(evt) {
                 console.log("alert!!");
         }
         if (evtK == 39) { //RIGHT
-            if (bPgdn) {
-                dnPage();
-                setSelectedIndex(1);
-            }
-            if (carpos == -1)
-                carpos = document.getElementById('txtPad').selectionEnd;
+            rightopt();
             return;
         }
         if (evtK == 37) { //LEFT
-            if (bPgup) {
-                upPage();
-                setSelectedIndex(1);
-            }
-            if (carpos == -1)
-                carpos = document.getElementById('txtPad').selectionEnd;
+            leftopt();
             return;
         }
     } else {
@@ -413,6 +404,223 @@ function txtPadKeyReleased(evt) {
     var evtK = evt.keyCode || evt.charCode;
     if (evtK == 16)
         shiftbool = false;
+}
+
+function rightopt() {
+    if (bPgdn) {
+        dnPage();
+        setSelectedIndex(1);
+    }
+    if (carpos == -1)
+        carpos = document.getElementById('txtPad').selectionEnd;
+    return;
+}
+
+function leftopt() {
+    if (bPgup) {
+        upPage();
+        setSelectedIndex(1);
+    }
+    if (carpos == -1)
+        carpos = document.getElementById('txtPad').selectionEnd;
+    return;
+}
+
+// Touch-input handler: mobile/touch keyboards don't reliably report keyCode on
+// keydown/keypress, so composition here is inferred by diffing txtPad's value
+// on 'keyup' instead of reading key events directly (contrast with
+// txtPadKeyPressed/txtPadKeyTyped, which handle the same job for physical
+// keyboards). Wired up conditionally in index.html based on touch support.
+function txtPadKeyInput(evt) {
+    var evtK = evt.keyCode || evt.charCode;
+    var txtPadEl = document.getElementById("txtPad");
+    var rubytypeEl = document.getElementById("rubytype");
+    var rubystr = rubytypeEl.textContent;
+
+    var curcaret = txtPadEl.selectionEnd;
+
+    var newtxtPadlength =
+        document.getElementById("txtPad").value.length;
+
+    // Max composition length
+    if ([...rubystr].length >= 12) {
+
+        contail = "";
+        conqueue = "";
+        conlenbuf = 0;
+
+        delList();
+
+        rubytypeEl.innerHTML = "";
+
+        lentype = 0;
+
+        return;
+    }
+
+    // BACKSPACE
+    if (newtxtPadlength < curtxtPadlength) {
+
+        if (curtxtPadlength - newtxtPadlength > 1) {
+
+            rubytypeEl.innerHTML = "";
+
+            listUpdate();
+
+            lentype = 0;
+
+            curtxtPadlength =
+                document.getElementById("txtPad").value.length;
+
+            return;
+        }
+
+        if (rubystr.length > 0) {
+
+            rubytypeEl.innerHTML =
+                rubystr.substring(0, rubystr.length - 1);
+
+            lentype--;
+
+        } else {
+
+            lentype = 0;
+        }
+
+        listUpdate();
+
+        curtxtPadlength =
+            document.getElementById("txtPad").value.length;
+
+        return;
+    }
+
+    // CHARACTER INPUT
+    if (newtxtPadlength > curtxtPadlength) {
+
+        var evtC =
+            document.getElementById("txtPad")
+            .value.substring(curcaret - 1, curcaret);
+
+        // ENTER
+        if (evtK == 13) {
+
+            rubytypeEl.innerHTML = "";
+
+            listUpdate();
+
+            lentype = 0;
+
+            curtxtPadlength =
+                document.getElementById("txtPad").value.length;
+
+            return;
+        }
+
+        var ind = selectedindex - 1;
+
+        // SPACE
+        if (evtC == ' ') {
+
+            txtPadEl.value =
+                txtPadEl.value.substring(0, curcaret - 1) +
+                txtPadEl.value.substring(
+                    curcaret,
+                    txtPadEl.value.length
+                );
+
+            txtPadEl.selectionStart =
+            txtPadEl.selectionEnd =
+                curcaret - 1;
+
+            if (optionlist.length != 0) {
+
+                evt.preventDefault();
+
+                var wordEl = document.getElementById("w" + selectedindex);
+
+                conlentmp =
+                    wordEl.textContent.length;
+
+                putWord(
+                    wordEl.textContent
+                );
+            }
+
+            curtxtPadlength =
+                document.getElementById("txtPad").value.length;
+
+            txtPadEl.focus();
+
+            return;
+        }
+
+        // Punctuation
+        else if (
+            (evtC == '.') ||
+            (evtC == ',')
+        ) {
+
+            txtPadEl.value =
+                txtPadEl.value.substring(0, curcaret - 1) +
+                evtC +
+                txtPadEl.value.substring(
+                    curcaret,
+                    txtPadEl.value.length
+                );
+
+            txtPadEl.selectionStart =
+            txtPadEl.selectionEnd =
+                curcaret - 1;
+
+            if (
+                (ind >= conrSz) &&
+                (ind < contrSz)
+            )
+                conlenbuf = conlentail;
+
+            if (ind >= contrSz)
+                conlenbuf = 0;
+
+            if (optionlist.length != 0) {
+
+                putWord(
+                    document.getElementById("w" + selectedindex).textContent
+                );
+            }
+
+            conqueue = "";
+            contail = "";
+            conlenbuf = 0;
+
+            lentype = 0;
+
+            txtPadEl.selectionStart =
+            txtPadEl.selectionEnd =
+                txtPadEl.selectionEnd + 1;
+
+            lentype++;
+
+            rubytypeEl.innerHTML =
+                typeChar(rubytypeEl.textContent, evtC);
+        }
+
+        // Normal character
+        else if (evtK != 8) {
+
+            lentype++;
+
+            rubytypeEl.innerHTML =
+                typeChar(rubytypeEl.textContent, evtC);
+        }
+
+        listUpdate();
+
+        curtxtPadlength =
+            document.getElementById("txtPad").value.length;
+
+        return;
+    }
 }
 
 function putWord(instr) {
